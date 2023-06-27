fddb892898e33ccdb858f577a7bbb29bbc5efee8
Sown13 <sonnh.buh@gmail.com>
2023-06-28 02:11:49 +0700

    should be done game layout

--- js/ajax/preview-display-method.js
+++ js/ajax/preview-display-method.js
@@ -4,10 +4,13 @@ function displayIcon(divElementId,imgElementSrc){
     let imgElement = document.createElement('img');
     imgElement.src = imgElementSrc;
     imgElement.className = divElement.className;
+    imgElement.id = divElement.id;
     imgElement.style.cssText = divElement.style.cssText;
     imgElement.style.cssFloat = divElement.style.cssFloat;
     divElement.parentNode.replaceChild(imgElement, divElement);
+    console.log(2222)
 }
+
 let char1Src = '../img/gif/gun-man.gif';
 let char2Src = '../img/gif/robber.gif';
 let char3Src = '../img/gif/crusader.gif';
@@ -18,6 +21,16 @@ displayIcon('char-1-inside',char1Src);
 displayIcon('char-2-inside',char2Src);
 displayIcon('char-3-inside',char3Src);
 displayIcon('char-4-inside',char4Src);
+
+function removeIcon(divElementId){
+    let divElement = document.getElementById(divElementId);
+    let imgElement = document.createElement('img');
+    imgElement.className = divElement.className;
+    imgElement.id = divElement.id;
+    imgElement.style.cssText = divElement.style.cssText;
+    imgElement.style.cssFloat = divElement.style.cssFloat;
+    divElement.parentNode.replaceChild(imgElement, divElement);
+}
 
 let inventoryButtonIcon = `../img/before-convert/Inventory_Icon.webp`;
 displayIcon('inventory-button-icon',inventoryButtonIcon)
@@ -43,3 +56,21 @@ function popupItemDetail(itemNumber,popupNumber){
 }
 popupItemDetail(1,1);
 popupItemDetail(2,2);
+
+
+let characterAction3 ='../img/before-convert/crusader-attack.webp';
+let enemyDefend1 = '../img/before-convert/hag-defend.webp';
+let audioElement = new Audio('../audio/crusader-attack-2.wav');
+function displayCharacterAction(characterIndex,characterActionSrc) {
+    audioElement.play();
+    removeIcon(`char-${characterIndex}-inside`);
+    displayIcon('character-action', characterActionSrc);
+    removeIcon('enemy-inside');
+    displayIcon('enemy-inside',enemyDefend1);
+    setTimeout(function() { displayIcon('char-3-inside', char3Src); }, 700);
+    setTimeout(function() { removeIcon('character-action'); }, 700);
+    setTimeout(function() { removeIcon('enemy-inside'); }, 700);
+    setTimeout(function() { displayIcon('enemy-inside',enemySrc)}, 700);
+}
+displayIcon(`character-avatar`,char1Src)
+displayIcon(`skill-detail-left`,`../img/before-convert/skill-2.webp`)
