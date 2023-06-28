5d51d6a3a3a6a30f6081c5ee97b98463b0dc9f87
Sown13 <sonnh.buh@gmail.com>
2023-06-28 08:22:12 +0700

    init some arrray

---
+++ js/ajax/game-logic.js
@@ -0,0 +1,27 @@
+let playerList = [];
+let playerDetailList = [];
+
+let alivePlayerList = [];
+let alivePlayerDetailList = [];
+
+let deadPlayerList = [];
+let deadPlayerDetailList = [];
+
+let enemyList = [];
+let enemyDetailList = [];
+
+let aliveEnemyList = [];
+let aliveEnemyDetailList = [];
+
+let deadEnemyList = [];
+let deadEnemyDetailList = [];
+
+let player1SkillList = [];
+let player2SkillList = [];
+let player3SkillList = [];
+let player4SkillList = [];
+
+let enemy1SkillList = [];
+let enemy2SkillList = [];
+let enemy3SkillList = [];
+let enemy4SkillList = [];
